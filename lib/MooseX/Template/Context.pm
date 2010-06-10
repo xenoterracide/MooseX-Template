d084746dab4d3a10e86cf02a3307813105678762
Caleb Cushing <xenoterracide@gmail.com>
2010-06-09 21:17:52 -0400

    add basic documentation to Template::Context
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/MooseX/Template/Context.pm
+++ lib/MooseX/Template/Context.pm
@@ -6,3 +6,17 @@ extends 'Template::Context';
 
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
 1;
+
+# ABSTRACT: Moose-ified TT Template::Context class
+=head1 SYNOPSIS
+
+	use MooseX::Template;
+
+	my $tt = MooseX::Template::Context->new;
+
+=head1 DESCRIPTION
+
+Moosified empty TT classes for applying Traits to. This is the
+Template::Context class
+
+=cut
